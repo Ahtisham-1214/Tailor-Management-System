e2b367224949d78cf18154cd274bb103eb939176
Ahtisham-1214 <ahtishamshaikh1214@gmail.com>
2025-03-24 22:20:07 +0500

    Customer Class

---
+++ Customer.java
@@ -0,0 +1,30 @@
+public class Customer {
+    private String name;
+    private String phoneNumber;
+
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public Customer(String name, String phoneNumber) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
